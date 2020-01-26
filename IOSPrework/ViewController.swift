9bbc14238649996821fb5f80c776049da7c9a08a
Kelly Dong <dongweiling27@gmail.com>
2020-01-26 08:33:38 -0500

    Initial Commit

---
+++ IOSPrework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSPrework
+//
+//  Created by Kelly Dong on 1/26/20.
+//  Copyright © 2020 Kelly Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
