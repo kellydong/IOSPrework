50427a96062cef323f89979d1f1e8fc36ce8ec02
Kelly Dong <dongweiling27@gmail.com>
2020-01-26 11:44:59 -0500

    finish Prework Project

--- IOSPrework/ViewController.swift
+++ IOSPrework/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var backgroundColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundColor = view.backgroundColor
     }
 
-
+    @IBAction func didTapButton(_ sender: Any) {
+        print("Hello")
+        textLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.orange
+    }
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+        if textField.text == "" {
+            textLabel.text = "Goodbye 👋"
+        }
+        else {
+            textLabel.text = textField.text
+        }
+        textField.text = ""
+        view.endEditing(true)
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLabel.text = "Hello From Kelly"
+        view.backgroundColor = backgroundColor
+        textLabel.textColor = UIColor.black
+    }
 }
 
